75d0c566f01cd474e295a72934fbcd9d2df28e5a
pierrerochet <pierre-rochet@outlook.fr>
2022-11-07 21:00:04 +0100

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Pierre Rochet",
   tagline: "Machine Learning Engineer",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  url: "https://pierrerochet.github.io",
+  baseUrl: "/personal-website/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
